f3158af8c6c8345bef87b78bf5ae63bb06094567
user1 <pohigarant@,ail.ru>
2026-03-25 10:40:30 +0300

    Add ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -3,12 +3,12 @@ from django.http import JsonResponse, Http404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from rest_framework import viewsets
 
-from .models import Category
+from .models import Category, Product, Buyer, Review
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, ProductSerializer, BuyerSerializer, ReviewSerializer
 
 
 def hello_view(request):
@@ -22,3 +22,19 @@ def hello_view(request):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class BuyerViewSet(viewsets.ModelViewSet):
+    queryset = Buyer.objects.all()
+    serializer_class = BuyerSerializer
+
+class BasketViewSet(viewsets.ModelViewSet):
+    queryset = Buyer.objects.all()
+    serializer_class = BuyerSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
